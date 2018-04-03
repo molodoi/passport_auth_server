d7a825b1c64c9fee67ea2645b8d35ea7a21347f2
Matthieu Lamerant <matthieu.lamerant@goandlive.tech>
2018-04-03 17:44:05 +0200

    Init

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Requests\RegisterRequest;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+class RegisterController extends Controller
+{
+
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(RegisterRequest $request)
+    {
+        User::create([
+            'name'     => $request['name'],
+            'email'    => $request['email'],
+            'password' => bcrypt($request['password']),
+        ]);
+        return response()->json([
+            'success' => true,
+            'message' => 'Register success.',
+        ]);
+    }
+}
